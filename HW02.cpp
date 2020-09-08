76639bb5f24654205f48a64f41b85f0f6baba2db
Sid <39922327+ranasid17@users.noreply.github.com>
2020-09-07 23:46:28 -0500

    Update HW02.cpp
    added comments to main(), formatted class member functions

--- HW02.cpp
+++ HW02.cpp
@@ -3,40 +3,26 @@ using namespace std;
 
 class rectangle{
    // set private attributes
-   public: 
+   private: 
     int width, length; 
    // define public methods 
    public: 
       // constructor
       rectangle(): width(0), length(0) { };  
       // mutators
-      void setWidth (int newVal) {
-         width = newVal; 
-      }
-      void setLength (int newVal) {
-         length = newVal; 
-      }
+      void setWidth (int newVal) { width = newVal; }
+      void setLength (int newVal) { length = newVal; }
       // accessors 
-      int getWidth() const {
-         return width; 
-      }
-      int getLength() const {
-         return width; 
-      }
-      // remaining member function declarations
-      int perimeter() {
-         return (width * 2) + (length * 2); 
-      }
-      int area() {
-         return (width * length); 
-      }
+      int getWidth() const { return width; }
+      int getLength() const { return width; }
+      // member function: calc perimeter of rectangle
+      int perimeter() { return (width * 2) + (length * 2); }
+      // member function: calc area of rectangle
+      int area() { return (width * length); }
+      // member function: determine if rectangle is also a square
       void Square_or_Rectangle(int width, int length){
-         if (width == length) {
-            cout << "SQUARE" << endl; 
-         }
-         else {
-            cout << "RECTANGLE" << endl; 
-         }
+         if (width == length) { out << "SQUARE" << endl; }
+         else { cout << "RECTANGLE" << endl; }
          return; 
       }
 };
@@ -47,15 +33,15 @@ int main() {
    while (width != 0) {
       // user input values 
       cin >> width >> length; 
-      // define new object
+      // define new object, rect from class rectangle
       rectangle rect; 
-      rect.setWidth(width); 
-      rect.setLength(length); 
+      rect.setWidth(width); // set width of rect from input width 
+      rect.setLength(length); // set length of rect from input length 
       // run functions
       cout << "Output" << endl;
-      rect.Square_or_Rectangle(width, length); 
-      cout << rect.area() << endl; 
-      cout << rect.perimeter() << endl; 
+      rect.Square_or_Rectangle(width, length); // output if given (w, l) == square
+      cout << rect.area() << endl; // output area of (w,l)
+      cout << rect.perimeter() << endl; // output perimeter of (w,l) 
       cout << endl; 
    }
    return 0;
