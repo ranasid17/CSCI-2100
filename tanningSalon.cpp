814c5c46f518d27287093f6be53985fbdff8b2bd
Sid <39922327+ranasid17@users.noreply.github.com>
2020-11-24 12:43:50 -0600

    Update tanningSalon.cpp
    fixed comments in background

--- tanningSalon.cpp
+++ tanningSalon.cpp
@@ -2,21 +2,21 @@
 #include <string>
 using namespace std;
 
-# Background 
-# 	The input consists of data for one or more salons, followed by a line containing the number 0 that signals the end of the input. 
-# 	Data for each salon is a single line containing a positive integer, representing the number of tanning beds in the salon, followed by a space, 
-#	followed by a sequence of uppercase letters. Letters in the sequence occur in pairs. 
-#	The first occurrence indicates the arrival of a customer, the second indicates the departure of that same customer. 
-#	No letter will occur in more than one pair. Customers who leave without tanning always depart before customers who are currently tanning. 
-#	There are at most 20 beds per salon.
-# Output
-#	For each salon, output a sentence telling how many customers, if any, walked away. Use the exact format shown below.
-# Example input:	Example output:
-# 2 ABBAJJKZKZ		All customers tanned successfully.
-# 3 GACCBDDBAGEE	1 customer(s) walked away.
-# 3 GACCBGDDBAEE	All customers tanned successfully.
-# 1 ABCBCA		2 customer(s) walked away.
-# 0	
+// Background 
+// The input consists of data for one or more salons, followed by a line containing the number 0 that signals the end of the input. 
+// 	Data for each salon is a single line containing a positive integer, representing the number of tanning beds in the salon, followed by a space, 
+//	followed by a sequence of uppercase letters. Letters in the sequence occur in pairs. 
+//	The first occurrence indicates the arrival of a customer, the second indicates the departure of that same customer. 
+//	No letter will occur in more than one pair. Customers who leave without tanning always depart before customers who are currently tanning. 
+//	There are at most 20 beds per salon.
+// Output
+//	For each salon, output a sentence telling how many customers, if any, walked away. Use the exact format shown below.
+// Example input:	Example output:
+// 2 ABBAJJKZKZ		All customers tanned successfully.
+// 3 GACCBDDBAGEE	1 customer(s) walked away.
+// 3 GACCBGDDBAEE	All customers tanned successfully.
+// 1 ABCBCA		2 customer(s) walked away.
+// 0	
 
 int
 main ()
